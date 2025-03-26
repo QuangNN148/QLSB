ab25c27f749b2cc8bd101e69b596395f781ce429
NNQ148 <nnq148@gmail.com>
2025-03-27 00:57:59 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
-import Button from '@/components/button'
-import '@/styles/styles.css'
+// import Button from '@/components/button'
+// import '@/styles/styles.css'
  
 export default function HomePage() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Button />
+      {/* <Button /> */}
     </div>
   )
 }
