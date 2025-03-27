db797cd2f50cec1941d8b6e68807a561a3046c32
NNQ148 <nnq148@gmail.com>
2025-03-28 00:18:22 +0700

    font

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 import Image from 'next/image';
+import {Inter} from 'next/font/google';
 import profilePic from '../public/clq.jpg';
 
+const inter = Inter({ subsets: ['latin'] });
+
 export default function Home() {
-  return <h1>Chào mừng đến trang chủ!</h1>,
-  <Image
-      src={profilePic} // Ảnh đã import
-      alt="Ảnh của tôi" // Mô tả ảnh (rất quan trọng cho SEO và accessibility)
-      width={1000}
-      height={600}
-    />
+  return (
+    <div>
+      <h1>Chào mừng đến trang chủ!</h1>,
+      <Image
+        src={profilePic} // Ảnh đã import
+        alt="Ảnh của tôi" // Mô tả ảnh (rất quan trọng cho SEO và accessibility)
+        width={1000}
+        height={600}
+      />
+    </div>
+  );
 }
