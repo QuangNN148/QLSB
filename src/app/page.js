213f5e2bbea28671abbd4b69a90ed16ecef3d120
NNQ148 <nnq148@gmail.com>
2025-03-27 13:46:02 +0700

    clq

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+import profilePic from '../public/clq.jpg';
+
 export default function Home() {
-  return <h1>Chào mừng đến trang chủ!</h1>;
+  return <h1>Chào mừng đến trang chủ!</h1>,
+  <Image
+      src={profilePic} // Ảnh đã import
+      alt="Ảnh của tôi" // Mô tả ảnh (rất quan trọng cho SEO và accessibility)
+      width={1000}
+      height={600}
+    />
 }
